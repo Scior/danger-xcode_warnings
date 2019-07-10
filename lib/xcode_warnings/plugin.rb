35ae243d8f2f7a5c18b8b17a28b80075c1b319f1
Scior <interval12@gmail.com>
2019-07-11 00:59:53 +0900

    Add sticky flag

--- lib/xcode_warnings/plugin.rb
+++ lib/xcode_warnings/plugin.rb
@@ -13,10 +13,10 @@ module Danger
     # @param [String] log_text Raw build log text.
     # @return [void]
     #
-    def analyze(log_text)
+    def analyze(log_text, sticky = true)
       parsed = LogParser.new.parse(log_text)
       parsed.each do |warning|
-        warn(warning[:message], file: warning[:path], line: warning[:line])
+        warn(warning[:message], sticky: sticky, file: warning[:path], line: warning[:line])
       end
       message "Detected #{parsed.count} build-time warnings." unless parsed.empty?
     end
@@ -25,10 +25,10 @@ module Danger
     # @param [String] file_path Path for the log file.
     # @return [void]
     #
-    def analyze_file(file_path)
+    def analyze_file(file_path, sticky = true)
       File.open(file_path) do |f|
         puts "Opened #{file_path}"
-        analyze(f.read)
+        analyze(f.read, sticky: sticky)
       end
     rescue Errno::ENOENT, Errno::EACCES => e
       puts "Couldn't open the file: #{e}"
